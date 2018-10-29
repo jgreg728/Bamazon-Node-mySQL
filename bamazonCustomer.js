b325a0c282d3a1b7261458602b2562b7c1732f05
jgreg728 <jjg070890@aol.com>
2018-10-29 00:56:44 -0400

    added pseudo

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -64,8 +64,7 @@ const searchAndBuy = () => {
       const selectedProduct = items.find(item => answer.productWeWant === item.product_name);
       console.log(selectedProduct);
 
-      if (answer.amountWeWant <= selectedProduct.stock_quantity);
-
+      if (answer.amountWeWant <= selectedProduct.stock_quantity);             
       }
     );
   })
@@ -85,7 +84,13 @@ const updateQuantity = (itemId, quantity) => {
   // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
   (err, result) => {
     if (err) throw err;
-    console.log("Sorry, there is not enough stock. Please request a quantity below what's available");
+    console.log("Insufficient quantity!");
     startBamazon();
   })
 }
+
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+
+// Once the update goes through, show the customer the total cost of their purchase.
